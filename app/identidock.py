f4aa552e5e832f9a360c26ace4cc1ded4e6f4a05
AlinaFirsova101 <alinafirsova178@gmail.com>
2021-01-17 02:37:55 +0300

    Initial commit

---
+++ app/identidock.py
@@ -0,0 +1,29 @@
+from flask import Flask, Response, request, render_template
+import requests
+import hashlib
+import redis
+
+app = Flask(__name__)
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
+default_name = 'Joe Bloggs'
+
+
+@app.route('/')
+def mainpage():
+    return render_template('index.html', name=default_name)
+
+
+@app.route('/monster/<name>')
+def get_identicon(name):
+
+    image = cache.get(name)
+    if image is None:
+        print ("Cache miss", flush=True)
+        r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+        image = r.content
+        cache.set(name, image)
+
+    return Response(image, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='9090')
