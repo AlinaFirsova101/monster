a2a33edeaeba29af4130f7f105713a6ab6da2604
AlinaFirsova101 <alinafirsova178@gmail.com>
2021-01-18 23:17:44 +0300

    Make test simpler

--- app/test_identidock.py
+++ app/test_identidock.py
@@ -10,17 +10,10 @@ class TestIdentify(unittest.TestCase):
     
 
     def test_get_mainpage(self):
-        page = self.app.post( "/", data=dict(name="Mary Smith"))
+        page = self.app.get( "/")
         assert page.status_code == 200
         assert 'Hello' in str( page.data )
-        assert 'Mary Smith' in str( page.data )
-
-    
-    def test_html_escaping(self):
-        page = self.app.post( "/", data=dict(name='"><b>TEST</b><!--') ) 
-        assert '<b>' not in str( page.data )
-
-        
+        assert 'Mary Smith' in str( page.data )   
         
 if __name__ == '__main__':
     unittest.main()
